de64f665a39ed79e0e7b4d1443a29a2b24f5e836
Walson Li <46850531+Walson124@users.noreply.github.com>
2022-02-25 13:23:40 -0500

    bubble paint component
    didn't add much, error in vscode is all

--- main/driver.java
+++ main/driver.java
@@ -15,19 +15,24 @@ public class driver {
         JFrame mainFrame = new JFrame();
         frameSetup(mainFrame);
 
-        JPanel mainPanel = new mainPanel();
+        mainPanel mainPanel = new mainPanel();
         panelSetup(mainFrame, mainPanel);
+        mainPanel.setBubbleArray(bubbleArray);
 
     }
 
     private static bubble[][] generateBubbles() {
-        int radius = 10;
+        int radius = 26;
         bubble[][] bubbleArray = new bubble[17][16];
         for (int i = 0; i < 17; i++) {
             for (int j = 0; j < 16; j++) {
+                int xShift = 10;
+                int yShift = 10;
+                if (j % 2 == 0)
+                    xShift += 15;
                 bubbleArray[i][j] = new bubble();
-                bubbleArray[i][j].setLocation(new Point(1, 2));
-                bubbleArray[i][j].setRadius(10);
+                bubbleArray[i][j].setLocation(new Point(30 * i + xShift, 30 * j + yShift));
+                bubbleArray[i][j].setRadius(radius);
                 bubbleArray[i][j].setColor(bubbleArray[i][j].randColor());
             }
         }
@@ -44,7 +49,7 @@ public class driver {
     private static void panelSetup(JFrame mainFrame, JPanel mainPanel) {
         mainPanel.setSize(mainFrame.getSize());
         mainFrame.getContentPane().add(mainPanel);
-        mainPanel.setBackground((new bubble()).randColor());
+        // mainPanel.setBackground((new bubble()).randColor());
     }
 
 }
